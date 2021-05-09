6b2e1e2c266b71a132c2668d6f099caf938d9ff9
Hoyame <zou.hoyame@gmail.com>
2021-05-09 23:13:10 +0200

    init: project

---
+++ src/client/index.ts
@@ -0,0 +1,45 @@
+import { CoraUI } from "./menu/menu"
+
+
+setImmediate(() => {
+    console.log('clientside', '')
+    SetWeatherTypePersist('XMAS')
+    NetworkOverrideClockTime(6, 34, 56)
+
+    CoraUI.openMenu({
+        name: 'Menu F5',
+        glare: true,
+        buttons: [
+            { name: 'Am', onClick: () => console.log(15641)},
+            { name: 'Am', onClick: () => {
+                console.log('sitterlan amonaguyem')
+            }},
+            { name: 'Am', onClick: () => console.log(54154)},
+            { name: 'Am', onClick: () => null},
+            { name: 'Am', onClick: () => null},
+            { name: 'Close menu', onClick: () => CoraUI.closeMenu()},
+            { name: 'Am', onClick: () => null},
+            { name: 'Am', onClick: () => null},
+            { name: 'Am', onClick: () => null},
+        ]
+    })
+
+})
+
+setTick(() => {
+    if (IsControlJustPressed(0, 38)) {
+        console.log("openemenu")
+        CoraUI.openMenu({
+            name: 'Menu E',
+            glare: true,
+            buttons: [
+                { name: 'Am', onClick: () => {
+                    console.log('sitterlan amonaguyem')
+                }},
+                { name: 'Close menu', onClick: () => CoraUI.closeMenu()},
+            ]
+        })
+    }
+})
+
+
